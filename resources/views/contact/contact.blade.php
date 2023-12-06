c7d587d35b889f513307643a80a7f58994234ee7
payal mundharikar <mundhrikarpayal@gmail.com>
2023-12-07 03:07:32 +0530

    chnagess

--- resources/views/contact/contact.blade.php
+++ resources/views/contact/contact.blade.php
@@ -3,39 +3,88 @@
 <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
 <html lang="en">
 
-<head>
+<head></head>
 
-</head>
 
 <body
-    style="background-color:#f5f5fb;font-family:-apple-system,BlinkMacSystemFont,&quot;Segoe UI&quot;,Roboto,Oxygen-Sans,Ubuntu,Cantarell,&quot;Helvetica Neue&quot;,sans-serif">
+    style="background-color:#fff;font-family:-apple-system,BlinkMacSystemFont,&quot;Segoe UI&quot;,Roboto,Oxygen-Sans,Ubuntu,Cantarell,&quot;Helvetica Neue&quot;,sans-serif">
     <table align="center" role="presentation" cellSpacing="0" cellPadding="0" border="0" width="100%"
-        style="max-width:37.5em;margin:0 auto;padding:20px 0 48px">
+        style="max-width:37.5em">
         <tr style="width:100%">
             <td>
+                <table style="padding:30px 20px" align="center" border="0" cellPadding="0" cellSpacing="0"
+                    role="presentation" width="100%">
+                    <tbody>
+                        <tr>
+                            <td>
+                                <img src="http://localhost/apmax1/public/assets/img/logo.png" height="60px"
+                                    width="180px"
+                                    style="display:block;outline:none;border:none;text-decoration:none" />
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table style="border:1px solid rgb(0,0,0, 0.1);border-radius:3px;overflow:hidden" align="center"
+                    border="0" cellPadding="0" cellSpacing="0" role="presentation" width="100%">
+                    <tbody>
+                        <tr>
+                            <td><img src="https://react-email-demo-ijnnx5hul-resend.vercel.app/static/yelp-header.png"
+                                    width="620"
+                                    style="display:block;outline:none;border:none;text-decoration:none" />
+                                <table width="100%" style="padding:20px 40px;padding-bottom:0" align="center"
+                                    role="presentation" cellSpacing="0" cellPadding="0" border="0">
+                                    <tbody style="width:100%">
+                                        <tr style="width:100%">
+                                            <td>
+                                                <h1 style="font-size:32px;font-weight:bold;text-align:center">Hi
+                                                    {{ $details['name'] }},
+                                                </h1>
+                                                <h2 style="font-size:26px;font-weight:bold;text-align:center">Thanks for
+                                                    contacting!</h2>
 
-                <img src="http://localhost/apmax1/public/assets/img/logo.png" height="50px" width="150px">
+                                                <p style="font-size:16px;line-height:24px;margin:16px 0"> This automatic
+                                                    reply is just to let you know we received your request and we’ll
+                                                    get back to you with
+                                                    a
+                                                    response as quickly as possible.</p>
+                                                <p
+                                                    style="font-size:16px;line-height:24px;margin:16px 0;margin-top:-5px">
+                                                    While you wait, you are welcome take a look at our Frequently Asked
+                                                    Questions or browse through
+                                                    our
+                                                    Knowledge base where you can find lots of guidance.
+                                                </p>
+                                                <P>
+                                                    support team. We look forward to getting you back in business!
+                                                </P>
 
-                <br>
+                                                <p>Thank you</p>
+                                                <p style="font-size:16px;line-height:26px;margin:16px 0">Best,<br />The
+                                                    adspaymax team</p>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                                <table width="100%" style="padding:20px 40px;padding-top:0" align="center"
+                                    role="presentation" cellSpacing="0" cellPadding="0" border="0">
 
-                Hi {{ $details['name'] }},
-                Thanks for contacting!<br>
-                This automatic reply is just to let you know we received your request <br>and we’ll get back to you with
-                a<br>
-                response as quickly as possible.<br>
-                While you wait, you are welcome take a look at our Frequently Asked <br>Questions or browse through
-                our<br>
-                Knowledge base where you can find lots of guidance.<br>
-                support team. We look forward to getting you back in business!
-                </p>
-                {{-- <p>Login : {{ $details['name'] }}</p> --}}
-
-                {{-- <p>Email : {{ $details['email'] }}</p> --}}
-
-                <p>Thank you</p>
-                <p style="font-size:16px;line-height:26px;margin:16px 0">Best,<br />The adspaymax team</p>
-                <hr style="width:100%;border:none;border-top:1px solid #eaeaea;border-color:#cccccc;margin:20px 0" />
-                <p style="font-size:12px;line-height:24px;margin:16px 0;color:#8898aa">adspaymax</p>
+                                </table>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table style="padding:45px 0 0 0" align="center" border="0" cellPadding="0" cellSpacing="0"
+                    role="presentation" width="100%">
+                    <tbody>
+                        <tr>
+                            <td><img src="https://react-email-demo-ijnnx5hul-resend.vercel.app/static/yelp-footer.png"
+                                    width="620"
+                                    style="display:block;outline:none;border:none;text-decoration:none" /></td>
+                        </tr>
+                    </tbody>
+                </table>
+                <p style="font-size:12px;line-height:24px;margin:16px 0;text-align:center;color:rgb(0,0,0, 0.7)">1850
+                    McCulloch Blvd N, Ste C1-234, Lake Havasu City, AZ. 86403. USA</p>
             </td>
         </tr>
     </table>
--- resources/views/email/mail.blade.php
+++ resources/views/email/mail.blade.php
@@ -3,34 +3,85 @@
 <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
 <html lang="en">
 
-<head>
+<head></head>
 
-</head>
 
 <body
-    style="background-color:#f5f5fb;font-family:-apple-system,BlinkMacSystemFont,&quot;Segoe UI&quot;,Roboto,Oxygen-Sans,Ubuntu,Cantarell,&quot;Helvetica Neue&quot;,sans-serif">
+    style="background-color:#fff;font-family:-apple-system,BlinkMacSystemFont,&quot;Segoe UI&quot;,Roboto,Oxygen-Sans,Ubuntu,Cantarell,&quot;Helvetica Neue&quot;,sans-serif">
     <table align="center" role="presentation" cellSpacing="0" cellPadding="0" border="0" width="100%"
-        style="max-width:37.5em;margin:0 auto;padding:20px 0 48px">
+        style="max-width:37.5em">
         <tr style="width:100%">
             <td>
+                <table style="padding:30px 20px" align="center" border="0" cellPadding="0" cellSpacing="0"
+                    role="presentation" width="100%">
+                    <tbody>
+                        <tr>
+                            <td>
+                                <img src="http://localhost/apmax1/public/assets/img/logo.png" height="60px"
+                                    width="180px"
+                                    style="display:block;outline:none;border:none;text-decoration:none" />
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table style="border:1px solid rgb(0,0,0, 0.1);border-radius:3px;overflow:hidden" align="center"
+                    border="0" cellPadding="0" cellSpacing="0" role="presentation" width="100%">
+                    <tbody>
+                        <tr>
+                            <td><img src="https://react-email-demo-ijnnx5hul-resend.vercel.app/static/yelp-header.png"
+                                    width="620"
+                                    style="display:block;outline:none;border:none;text-decoration:none" />
+                                <table width="100%" style="padding:20px 40px;padding-bottom:0" align="center"
+                                    role="presentation" cellSpacing="0" cellPadding="0" border="0">
+                                    <tbody style="width:100%">
+                                        <tr style="width:100%">
+                                            <td>
+                                                <h1 style="font-size:32px;font-weight:bold;text-align:center">Hi
+                                                    {{ $details['name'] }},
+                                                </h1>
+                                                <h2 style="font-size:26px;font-weight:bold;text-align:center">Welcome to
+                                                    adspaymax,<br>
+                                                    You've successfully signed up for our services</h2>
 
-                <img src="http://localhost/apmax1/public/assets/img/logo.png" height="50px" width="150px">
-                <p style="font-size:16px;line-height:26px;margin:16px 0">Welcome to adspaymax,<br>
-                    Hi {{ $details['name'] }}, You've successfully signed up for our services</p>
-                <p>
+                                                <p style="font-size:16px;line-height:24px;margin:16px 0">Your New
+                                                    Account</p>
+                                                <p
+                                                    style="font-size:16px;line-height:24px;margin:16px 0;margin-top:-5px">
+                                                <p>Login : {{ $details['name'] }}</p>
+                                                <p>Email : {{ $details['email'] }}</p>
+                                                <p>Password : {{ $details['Password'] }}</p>
+                                                <p>Store Name : {{ $details['store'] }}</p>
+                                                <p>Address : {{ $details['Address'] }}</p>
+                                                </p>
 
-                    your new account
-                </p>
-                <p>Login : {{ $details['name'] }}</p>
 
-                <p>Email : {{ $details['email'] }}</p>
-                <p>Password : {{ $details['Password'] }}</p>
-                <p>Store Name : {{ $details['store'] }}</p>
-                <p>Address : {{ $details['Address'] }}</p>
-                <p>Thank you</p>
-                <p style="font-size:16px;line-height:26px;margin:16px 0">Best,<br />The adspaymax team</p>
-                <hr style="width:100%;border:none;border-top:1px solid #eaeaea;border-color:#cccccc;margin:20px 0" />
-                <p style="font-size:12px;line-height:24px;margin:16px 0;color:#8898aa">adspaymax</p>
+                                                <p>Thank you</p>
+                                                <p style="font-size:16px;line-height:26px;margin:16px 0">Best,<br />The
+                                                    adspaymax team</p>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                                <table width="100%" style="padding:20px 40px;padding-top:0" align="center"
+                                    role="presentation" cellSpacing="0" cellPadding="0" border="0">
+
+                                </table>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table style="padding:45px 0 0 0" align="center" border="0" cellPadding="0" cellSpacing="0"
+                    role="presentation" width="100%">
+                    <tbody>
+                        <tr>
+                            <td><img src="https://react-email-demo-ijnnx5hul-resend.vercel.app/static/yelp-footer.png"
+                                    width="620"
+                                    style="display:block;outline:none;border:none;text-decoration:none" /></td>
+                        </tr>
+                    </tbody>
+                </table>
+                <p style="font-size:12px;line-height:24px;margin:16px 0;text-align:center;color:rgb(0,0,0, 0.7)">1850
+                    McCulloch Blvd N, Ste C1-234, Lake Havasu City, AZ. 86403. USA</p>
             </td>
         </tr>
     </table>
