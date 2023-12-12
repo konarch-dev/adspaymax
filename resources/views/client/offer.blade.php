f3acc61c4f835a9206596e3a6f61ee160931f135
payal mundharikar <mundhrikarpayal@gmail.com>
2023-12-12 22:20:52 +0530

    changess

--- resources/views/client/offer.blade.php
+++ resources/views/client/offer.blade.php
@@ -5,6 +5,26 @@
 <head>
 
     <style>
+        .video {
+            position: relative;
+            width: 95%;
+            overflow: hidden;
+            padding-top: 56.25%;
+            margin-left: 30px;
+            ;
+        }
+
+        .responsive-iframe {
+            position: absolute;
+            top: 0;
+            left: 0;
+            bottom: 0;
+            right: 0;
+            width: 100%;
+            height: 100%;
+            border: none;
+        }
+
         h5 {
             color: black;
         }
@@ -186,7 +206,7 @@
                         <div class="info">
                             <h4 class="text-center">DIGITAL MAGAZINE</h4><br>
                             <img style="max-width: 100%;height: auto;object-fit: contain;display: block;text-align: center"
-                                src="{{ url('/public/assets/img/all/offer1.jpg') }}" alt=""><br>
+                                src="{{ asset('assets/img/all/offer1.jpg') }}" alt=""><br>
                             <p
                                 style="font-size: 20px;font-family: ABeeZee;color: black;text-align: center;
                 ">
@@ -208,7 +228,7 @@
 
                         <h4 class="text-center">ADSPAYMAX・POCKET BOOK COUPONS</h4><br>
                         <img style="max-width: 100%;height: auto;object-fit: contain;display: block;text-align: center"
-                            src="{{ url('/public/assets/img/all/offer2.jpg') }}" alt=""><br>
+                            src="{{ asset('assets/img/all/offer2.jpg') }}" alt=""><br>
                         <p
                             style="font-size: 20px;font-family: ABeeZee;color: black;text-align: center;
                             ">
@@ -330,9 +350,8 @@
                             MAX offers exclusive postcards mailed only to the buyers you’ve never met, on a monthly or
                             weekly affordable basis.
                             NO WASTE・HUGE ROI・EXCLUSIVITY
-                        </p> <br><br>
+                        </p>
                     </div>
-
                     <div class="row">
                         <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
                             <img src="{{ url('/public/assets/img/all/m1.jpg') }}" alt="">
@@ -347,7 +366,7 @@
                                 style="font-size: 18px;color: #0f0f0f;text-align: left;font-family:ABeeZee;font-weight:400;font-style:normal">
                                 DIGITAL ADSPAYMAX COUPON BOOK</h2><br>
                             <h2
-                                style="font-size: 13px;color: #dd3333;text-align: left;;font-weight:600;font-style:normal;font-family: serif;">
+                                style="font-size: 14px;color: #dd3333;text-align: left;;font-weight:600;font-style:normal;font-family: serif;">
                                 A monthly pre-paid plan at a price less than a cup of coffee per day</h2><br>
                             <ol
                                 style="    unicode-bidi: isolate;
@@ -394,7 +413,7 @@
                                 style="font-size: 18px;color: #0f0f0f;text-align: left;font-family:ABeeZee;font-weight:400;font-style:normal">
                                 DIGITAL ADSPAYMAX MAGAZINE</h2><br>
                             <h2
-                                style="font-size: 13px;color: #dd3333;text-align: left;;font-weight:600;font-style:normal;font-family: serif;">
+                                style="font-size: 14px;color: #dd3333;text-align: left;;font-weight:600;font-style:normal;font-family: serif;">
                                 A monthly pre-paid plan at a price less than an expresso coffee per day</h2><br>
                             <ol
                                 style="    unicode-bidi: isolate;
@@ -428,6 +447,7 @@
                         </div>
                     </div>
                     <br>
+
 
                     <div class="row">
                         <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
@@ -538,13 +558,10 @@
             MESSAGE MOVES・SO DO THEIR
             EYES</div>
         <br>
-        <div style=" display: block;z-index: 10;
-        position: relative;text-align: center;">
-            <iframe style="width: 1140px;
-            height: 641px;text-align: center"
-                src="http://www.youtube.com/embed/oNVaEkuFuiU">
-            </iframe>
+        <div class="video">
 
+            <iframe class="responsive-iframe" src="http://www.youtube.com/embed/oNVaEkuFuiU">
+            </iframe>
         </div>
         <br>
         @include('client/master/footer')
